523245403e5fe1ac234b628e54eb3d85c1a6b356
dankogrg <dankogrg@gmail.com>
2024-03-23 15:31:35 +0100

    added local storage and background color option, mssg display position modified

--- script.js
+++ script.js
@@ -2,21 +2,51 @@ const chatDisplay = document.querySelector("#chatdisplay");
 const chatInput = document.querySelector("#chatinput");
 const message = document.querySelector("#message");
 const button = document.querySelector("#button");
+const color = document.querySelector("#color");
 const userModal = document.querySelector("#usermodal");
 const userName = document.querySelector("#username");
 const userModalObj = new bootstrap.Modal(userModal, {});
 
 let user;
+let chat = {
+    chatLog: [],
+};
 
 userModal.addEventListener("submit", handleUser);
 message.addEventListener("keyup", handleinput);
 button.addEventListener("click", handleinput);
 
+onCahtLoad();
+function onCahtLoad() {
+    const records = localStorage.getItem("chat");
+    if (records) {
+        chat = JSON.parse(records);
+        chat.chatLog.forEach((chatMessage) => {
+            addToChatLog(chat.user, chat.color, chatMessage);
+        });
+    } else {
+        window.addEventListener("load", () => {
+            userModalObj.show();
+        });
+    }
+}
+
 function handleUser(e) {
     e.preventDefault();
-    user = userName.value;
 
+    chat.user = userName.value;
+    chat.color = color.value;
     userModalObj.hide();
+}
+
+function addToChatLog(user, color, message) {
+    const div = document.createElement("div");
+    div.className = "divclass";
+
+    div.innerHTML = `<p>${user}: ${message}</p>`;
+    div.style.backgroundColor = color;
+    chatDisplay.append(div);
+    chatInput.reset();
 }
 
 function handleinput(e) {
@@ -24,16 +54,9 @@ function handleinput(e) {
     if (e.key != "Enter" && e.key != undefined) {
         return;
     }
-    console.log(message.value);
 
-    const div = document.createElement("div");
-    div.className = "divclass";
-
-    div.innerHTML = `<p>${user}: ${message.value}</p>`;
-    chatDisplay.append(div);
-    chatInput.reset();
+    chat.chatLog.push(message.value);
+    console.log(chat);
+    localStorage.setItem("chat", JSON.stringify(chat));
+    addToChatLog(chat.user, chat.color, message.value);
 }
-
-window.addEventListener("load", () => {
-    userModalObj.show();
-});
